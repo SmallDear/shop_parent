1cf5df027c22d76b1198b607600a18d3b26f3db1
SmallDear <15910442549@163.com>
2019-06-21 15:51:31 +0800

    ''

--- shop_service_impl/service_member/src/main/java/feign/WxServiceFeign.java
+++ shop_service_impl/service_member/src/main/java/feign/WxServiceFeign.java
@@ -3,6 +3,9 @@ package feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import service.WxService;
 
+/**
+ * 会员调用 微信接口
+ */
 @FeignClient(name = "brhds-weixin")
 public interface WxServiceFeign extends WxService {
 
